96d5f5759b91aa24d0430086102d95b5f35dd14e
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-06-05 15:21:06 -0400

    adding subplots

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import sklearn
 
 data = pd.read_csv('data.csv')
 #this line gets the percentage of yes answers for each type of social media
+fig, axs = plt.subplots(2)
+fig.suptitle('Info')
 percents = [len(data[data['web1a'] == 1])/len(data), len(data[data['web1b'] == 1])/len(data), len(data[data['web1c'] == 1])/len(data), len(data[data['web1d'] == 1])/len(data), len(data[data['web1e'] == 1])/len(data), len(data[data['web1f'] == 1])/len(data), len(data[data['web1g'] == 1])/len(data), len(data[data['web1h'] == 1])/len(data)]
 #x axis labels
 labels = ['Twitter','Instagram','Facebook','Snapchat','YouTube','WhatsApp','Pinterest','LinkedIn']
 #sets the size of the graph
 plt.figure(figsize=(16,6))
 #graph, with the x axis labels and y values
-plt.bar(labels, percents)
+axs[0].bar(labels, percents)
 #labels
 plt.xlabel('Platforms', fontsize=12, labelpad=5)
 plt.ylabel('Frequency', fontsize=12, labelpad=5)
@@ -26,7 +28,7 @@ labels2 = ['Phone','Tablet','Desktop/Laptop','Game Console']
 #sets the size of the graph
 plt.figure(figsize=(16,6))
 #graph, with the x axis labels and y values
-plt.bar(labels2, percents2)
+axs[1].bar(labels2, percents2)
 #labels
 plt.xlabel('Platform Used', fontsize=12, labelpad=5)
 plt.ylabel('Frequency', fontsize=12, labelpad=5)
@@ -36,7 +38,6 @@ plt.title('Platform Ownership')
 
 #hi people
 #hello world
-
 
 
 plt.show()
