f3ba9d0a5200c2825b8c8498458ac4aa6d85af52
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-06-05 15:31:45 -0400

    actually fixed subplots

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sklearn
 
 data = pd.read_csv('data.csv')
 #this line gets the percentage of yes answers for each type of social media
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(2, figsize=(16,8))
 fig.suptitle('Info')
 percents = [len(data[data['web1a'] == 1])/len(data), len(data[data['web1b'] == 1])/len(data), len(data[data['web1c'] == 1])/len(data), len(data[data['web1d'] == 1])/len(data), len(data[data['web1e'] == 1])/len(data), len(data[data['web1f'] == 1])/len(data), len(data[data['web1g'] == 1])/len(data), len(data[data['web1h'] == 1])/len(data)]
 #x axis labels
@@ -27,7 +27,7 @@ labels2 = ['Phone','Tablet','Desktop/Laptop','Game Console']
 axs[1].bar(labels2, percents2)
 #labels
 axs[1].set_xlabel('Platform Used', fontsize=12, labelpad=5)
-axs[0].set_ylabel('Frequency', fontsize=12, labelpad=5)
+axs[1].set_ylabel('Frequency', fontsize=12, labelpad=5)
 #title
 axs[0].set_title('Platform Ownership')
 #used to show to graph
