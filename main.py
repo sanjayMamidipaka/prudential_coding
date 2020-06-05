658a477b44beff57d078e8aa9ba930a8b6c03cf2
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-06-05 14:53:26 -0400

    Co-authored-by: Perplexiz <aneesh.sharma@mcvts.org> Co-authored-by: Jaxmano <47786485+Jaxmano@users.noreply.github.com> Co-authored-by: chris1028h <chris1028h@gmail.com> Co-authored-by: maliktragna <malik.tragna@mcvts.org>

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import sklearn
 
 data = pd.read_csv('data.csv')
-#print(np.nanmean(data['sns2d']))
-#print(len(data[data['snsint2'] == 1])/len(data), len(data[data['snsint2'] == 2])/len(data))
 #this line gets the percentage of yes answers for each type of social media
 percents = [len(data[data['web1a'] == 1])/len(data), len(data[data['web1b'] == 1])/len(data), len(data[data['web1c'] == 1])/len(data), len(data[data['web1d'] == 1])/len(data), len(data[data['web1e'] == 1])/len(data), len(data[data['web1f'] == 1])/len(data), len(data[data['web1g'] == 1])/len(data), len(data[data['web1h'] == 1])/len(data)]
 #x axis labels
@@ -21,4 +19,27 @@ plt.ylabel('Frequency', fontsize=12, labelpad=5)
 #title
 plt.title('Social Media Usage')
 #used to show to graph
+
+percents2 = [len(data[data['device1b'] == 1])/len(data), len(data[data['device1c'] == 1])/len(data), len(data[data['device1d'] == 1])/len(data)]
+#x axis labels
+labels2 = ['Tablet','Desktop/Laptop','Game Console']
+#sets the size of the graph
+plt.figure(figsize=(16,6))
+#graph, with the x axis labels and y values
+plt.bar(labels2, percents2)
+#labels
+plt.xlabel('Platform Used', fontsize=12, labelpad=5)
+plt.ylabel('Frequency', fontsize=12, labelpad=5)
+#title
+plt.title('Platform Ownership')
+#used to show to graph
+
+
+
+
+
+
+
+
 plt.show()
+
